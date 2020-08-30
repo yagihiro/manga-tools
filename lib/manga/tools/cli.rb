e7efba9eaa2498c46a12dd409ea4d2709e9b65b0
Hiroki Yagita <yagihiro@gmail.com>
2020-08-30 23:14:35 +0900

    add follow-list command

--- lib/manga/tools/cli.rb
+++ lib/manga/tools/cli.rb
@@ -9,12 +9,14 @@ module Manga
   module Tools
     # CLI class
     class CLI < Thor
+      HOST_OPTION = { aliases: '-h', desc: 'Specifies the host to connect to for development' }.freeze
+
       def self.exit_on_failure?
         true
       end
 
       desc 'search WORD', 'Search titles for a given WORD'
-      method_option :host, aliases: '-h', desc: 'Specifies the host to connect to for development'
+      method_option :host, HOST_OPTION
       def search(word)
         client = Manga::Tools::Client.new
         results = client.search(word, options)
@@ -22,11 +24,19 @@ module Manga
       end
 
       desc 'follow KEY', 'Follow a title for a given follow KEY'
-      method_option :host, aliases: '-h', desc: 'Specifies the host to connect to for development'
+      method_option :host, HOST_OPTION
       def follow(key)
         client = Manga::Tools::Client.new
         results = client.follow(key, options)
         Manga::Tools::Formatter.display(:follow, key, results)
+      end
+
+      desc 'follow-list', 'Displays the follow list'
+      method_option :host, HOST_OPTION
+      def follow_list
+        client = Manga::Tools::Client.new
+        results = client.follow_list(options)
+        Manga::Tools::Formatter.display(:follow_list, results)
       end
     end
   end
